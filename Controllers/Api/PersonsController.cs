9537008e2ba50b5ea39b1c8055d5267a772e672b
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-09-03 20:21:49 +0100

    creating the GetPerson & CreatePerson Actions

---
+++ Controllers/Api/PersonsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RegFormApi.Model;
+
+namespace RegFormApi.Controllers.Api
+{
+    [Route("api/[persons]")]
+    [ApiController]
+    public class PersonsController : Controller
+    {
+        private readonly RegFormApiDbContext context;
+
+        public PersonsController( RegFormApiDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetPersons()
+        {
+            var personDb = context.Persons.ToList();
+
+            return Ok(personDb);
+        }
+
+        [HttpPost]
+        public IActionResult CreatePerson(Person person)
+        {
+            context.Add(person);
+            context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
