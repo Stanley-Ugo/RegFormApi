6cc4fc3f4fd04c5e86655205c63be692184bd623
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-09-03 19:44:37 +0100

    adding a database connectionString

--- Startup.cs
+++ Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RegFormApi.Model;
 
 namespace RegFormApi
 {
@@ -25,6 +27,7 @@ namespace RegFormApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<RegFormApiDbContext>(options => options.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB; database=regformapi; Trusted_Connection=True;"));
             services.AddControllers();
         }
 
